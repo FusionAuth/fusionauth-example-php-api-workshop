1d47d4855b53e2decb23f2df1a19fa55261a443e
Dan Moore <dan@fusionauth.io>
2023-10-16 16:20:09 -0600

    add solution

--- jwts/verify_jwt.php
+++ jwts/verify_jwt.php
@@ -6,7 +6,7 @@ use \Firebase\JWT\Key;
 
 $key = "hello php-folk!!";
 
-// TODO
+$jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJmdXNpb25hdXRoLmlvIiwiZXhwIjoxNzIzNDE0NzU0LCJhdWQiOiIyMzhkNDc5My03MGRlLTQxODMtOTcwNy00OGVkOGVjZDE5ZDkiLCJzdWIiOiIxOTAxNmI3My0zZmZhLTRiMjYtODBkOC1hYTkyODc3Mzg2NzciLCJuYW1lIjoiRGFuIE1vb3JlIiwicm9sZXMiOlsiUkVUUklFVkVfSk9LRVMiXX0.TxHnvKhSCG6Op4G2ObAxc02McMWAeFfAlkhp9kuyvsQ";
 
 $decoded = JWT::decode($jwt, new Key($key, 'HS256'));
 
---
+++ jwts/verify_jwt_unsolved.php
@@ -0,0 +1,15 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+use \Firebase\JWT\JWT;
+use \Firebase\JWT\Key;
+
+$key = "hello php-folk!!";
+
+// TODO 
+
+$decoded = JWT::decode($jwt, new Key($key, 'HS256'));
+
+print_r($decoded);
+
+?>
